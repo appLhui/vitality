11af81add20e065f7c1daf2d16c34e41be525287
Li Hui <applelihui@icloud.com>
2013-05-02 17:18:57 +0800

    修改 完善login 页面 支持国际化

---
+++ src/main/webapp/src/modules/base_js/login.js
@@ -0,0 +1,33 @@
+(function() {
+  define(function(require) {
+    require('plug-in/validationEngine');
+    return $(function() {
+      var AppView;
+
+      AppView = Backbone.View.extend({
+        el: $('#login'),
+        events: {
+          "change select.language_select": "onlanguageChange"
+        },
+        initialize: function() {
+          this.language_select = this.$('select.language_select');
+          this.form = this.$('form');
+          this.url = window.location.href;
+          return this.render();
+        },
+        render: function() {
+          this.language_select.find('option[value="' + this.url.split('/')[5] + '"]').attr('selected', 'selected');
+          this.form.validationEngine();
+          return this;
+        },
+        onlanguageChange: function(e) {
+          top.location = '../../html/' + e.target.value + '/login.html';
+          return this;
+        }
+      });
+      new AppView();
+      return this;
+    });
+  });
+
+}).call(this);
---
+++ src/main/webapp/src/modules/coffee/login.coffee
@@ -0,0 +1,22 @@
+define (require) ->
+  require 'plug-in/validationEngine'
+  $ ->
+    AppView = Backbone.View.extend
+      el:$('#login')
+      events:
+        "change select.language_select": "onlanguageChange"
+      initialize: ->
+        @language_select=@$ 'select.language_select'
+        @form=@$ 'form'
+        @url=window.location.href
+        @render()
+      render: ->
+        @language_select.find('option[value="'+@url.split('/')[5]+'"]').attr 'selected','selected'
+        @form.validationEngine();
+        @
+      onlanguageChange:(e) ->
+        top.location='../../html/'+e.target.value+'/login.html'
+        @
+
+    new AppView()
+    @
